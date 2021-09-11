385ed99ce01a6f36c82ba66b790686f0e6222a1a
hasmurat <muratkangal0101@gmail.com>
2021-09-11 13:35:25 +0300

    initial commit

---
+++ src/test/java/tests/HelloTest.java
@@ -0,0 +1,7 @@
+package tests;
+
+public class HelloTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Test");
+    }
+}
---
+++ src/test/java/tests/MyFirstTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstTest {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
+    }
+}
